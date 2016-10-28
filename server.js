6bdb5055a94f8047229ec3d16b0c54adc5051895
Mario Araya Romero <arayaromero@gmail.com>
2016-10-28 12:19:59 -0300

    se escucha archivo con "chockidar" y se emite contenido

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var express = require('express');
 var app = express();
 var server = require("http").Server(app);
 var io = require('socket.io')(server);
-//var exphbs  = require('express3-handlebars');
-
-// app.engine('handlebars', exphbs({defaultLayout: '_layout'}));
-// app.set('view engine', 'handlebars');
+var chokidar = require('chokidar');
+var fs = require('fs');
 
 app.use('/socket.io', express.static(__dirname + 'node_modules/socket.io-client/'));
 
@@ -13,9 +11,18 @@ app.get('/', function(req, res){
     res.sendfile(__dirname + '/index.html')
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world', desde: 'el server' });
-  socket.on('my other event', function (data) {
+var watcher = chokidar.watch('texto.txt');
+
+io.on('connection', function (socket) { 
+  watcher.on('change', () => { 
+     fs.readFile('texto.txt', 'utf8', (err, data) => {
+      if(err) throw err;
+      console.log(data);
+      socket.emit('data cambiada', {dataCambiada: data});
+    })
+  });
+  
+  socket.on('haciaElServer', function (data) {
     console.log(data);
   });
 });
